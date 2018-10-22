fb071937a45485f4ee04403ceabaac24c5a34d79
Zohaib Naqvi (AUT) <zohaib@am5.net>
2018-10-22 11:04:14 +0500

    Add project files.

---
+++ Fasetto/ViewModel/BaseClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using PropertyChanged;
+namespace Fasetto
+{
+    /// <summary>
+    /// A base view model that fires property changed event as needed 
+    /// </summary>
+    [ImplementPropertyChanged]
+    public class BaseClass : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// the event that is fired when any child property changes
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
---
+++ Fasetto/ViewModel/RelayCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Fasetto
+{
+    /// <summary>
+    /// A basic Command that can run an action
+    /// </summary>
+
+    public class RelayCommand : ICommand
+    {
+        #region PrivateMemebers
+        // Action to run
+        private Action mAction;
+        #endregion
+
+        #region PublicEventHandler
+        /// <summary>
+        /// the event fired when they <see cref="CanExecute(object)"/> value has changed
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Default Contructor
+        /// </summary>
+        public RelayCommand(Action action) => mAction = action;
+        #endregion
+
+
+        /// <summary>
+        /// A relay command can always execute
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            mAction();
+        }
+    }
+}
