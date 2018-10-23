fcea10e7c26be5490aea0bee6eff21ba3813eb0d
Xzib <syedzohaibali94@gmail.com>
2018-10-23 17:30:29 +0500

    ADding window

--- Fasetto/ViewModel/Window/WindowViewModel.cs
+++ Fasetto/ViewModel/Window/WindowViewModel.cs
@@ -10,18 +10,91 @@ namespace Fasetto
     public class WindowViewModel : BaseClass
     {
 
+
+        #region Private Members
+
         /// <summary>
         /// The Window this view model controls
         /// </summary>
-        #region Private Members
-
         private Window mWindow;
 
+        /// <summary>
+        /// The Size of the outerMargin
+        /// </summary>
+        private int mOuterMarginSize=10;
+
+        /// <summary>
+        /// The radius of the window
+        /// </summary>
+        private int mWindowRadius=10;
+
         #endregion
 
+
+
+
         #region Public Properties
-        public string Test { get; set; }
+
+        /// <summary>
+        /// The size of the resize border around the window
+        /// </summary
+        public int ResizeBorder { get; set; } = 6;
+
+        /// <summary>
+        /// The size of the resizeborder taking into account the outer margin
+        /// </summary>
+        public Thickness ResizeBorederThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+        
+        /// <summary>
+        /// The property to set and assign values to outerMargin
+        /// </summary>
+        public int OuterMarginSize
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
+            }
+
+            set
+            {
+                mOuterMarginSize = value;
+            }
+        }
+
+        /// <summary>
+        /// The property to set and assign values to outerMargin
+        /// </summary>
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize); } }
+
+        /// <summary>
+        /// Set Window Radius
+        /// </summary>
+        public int WindowRadius
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
+            }
+
+            set
+            {
+                mWindowRadius = value;
+            }
+        }
+
+        /// <summary>
+        /// Set Corner Window Radius
+        /// </summary>
+        public CornerRadius WindowCornerRadius{ get { return new CornerRadius(WindowRadius); }  }
+
+        /// <summary>
+        /// The height of the CaptionBar
+        /// </summary>
+        public int TitleHeight { get; set; } = 42;
+
+
         #endregion
+
 
         /// <summary>
         /// Default Constructor
@@ -33,6 +106,15 @@ namespace Fasetto
         public WindowViewModel(Window window)
         {
             mWindow = window;
+            mWindow.StateChanged += (sender, e) =>
+            {
+                OnPropertyChanged(nameof(ResizeBorederThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
+
         }
         #endregion
     }
