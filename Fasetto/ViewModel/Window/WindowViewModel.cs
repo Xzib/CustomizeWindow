061a5a48752ebfa86a579286813cdab05f6006dd
Xzib <syedzohaibali94@gmail.com>
2018-10-25 13:54:36 +0500

    Added helper method

--- Fasetto/ViewModel/Window/WindowViewModel.cs
+++ Fasetto/ViewModel/Window/WindowViewModel.cs
@@ -136,24 +136,10 @@ namespace Fasetto
         #endregion
 
         #region PrivateHelpers
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool GetCursorPos(ref Win32Point pt);
-        [StructLayout(LayoutKind.Sequential)]
-
-        internal struct Win32Point
+        private Point GetMousePosition()
         {
-            public Int32 X;
-            public Int32 Y;
-
-        }
-
-        private static Point GetMousePosition()
-        {
-            Win32Point w32Mouse = new Win32Point();
-            GetCursorPos(ref w32Mouse);
-            return new Point(w32Mouse.X, w32Mouse.Y);
-        }
+            var pos = Mouse.GetPosition(mWindow);
+            return new Point(pos.X+mWindow.Left, pos.Y+mWindow.Top);       }
 
 
         #endregion
