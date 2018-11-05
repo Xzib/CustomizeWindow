330229a8abb917be7a87b16935ea5c1bc5510dfb
Xzib <syedzohaibali94@gmail.com>
2018-11-05 17:28:48 +0500

    added all

---
+++ Fasetto/AttachedProperties/BaseAttachedProperties.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Fasetto
+{
+    public abstract class BaseAttachedProperties<Parent, Property>
+    {
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.RegisterAttached
+            ("Value", typeof(bool), typeof(BaseAttachedProperties<Parent, Property>), new PropertyMetadata());
+            
+    }
+}
