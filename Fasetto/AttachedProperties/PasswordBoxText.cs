f19af9538b260e6791986656e24b3abf9a24d853
Xzib <syedzohaibali94@gmail.com>
2018-11-05 10:17:31 +0500

    Added Password Visibility text

--- Fasetto/AttachedProperties/PasswordBoxText.cs
+++ Fasetto/AttachedProperties/PasswordBoxText.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
-namespace Fasetto.AttachedProperties
+namespace Fasetto
 {
     public class PasswordBoxText
     {
@@ -16,7 +11,25 @@ namespace Fasetto.AttachedProperties
 
         private static void onMonitorPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
+            var password = (d as PasswordBox);
+            if (password == null)
+                return;
+
+            password.PasswordChanged -= Password_PasswordChanged;
+
+
+            if ((bool)e.NewValue)
+            {
+                SetHasText(password);
+                password.PasswordChanged += Password_PasswordChanged;
+            }
             
+
+        }
+
+        private static void Password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            SetHasText((PasswordBox)sender);
         }
 
         public static void SetMonitorPassword(PasswordBox element, bool value)
